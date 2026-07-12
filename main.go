edc4d78d2e97ca0e964f5a9db90bd4929dd80030
roushan-sheik <roushansheik@gmail.com>
2026-07-12 23:05:35 +0600

    feat: get and post users

--- main.go
+++ main.go
@@ -1,24 +1,73 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+var users = []User{
+	{
+		ID:    1,
+		Name:  "Roushan",
+		Email: "roushansheik@gmail.com",
+	},
+	{
+		ID:    2,
+		Name:  "Rohim",
+		Email: "rohim@gmail.com",
+	},
+	{
+		ID:    3,
+		Name:  "Karim",
+		Email: "karim@gmail.com",
+	},
+}  
+
 func main(){
 
-	http.HandleFunc("/", rootHandler)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/",rootHandler)
+	mux.HandleFunc("/health", healthChecker)
+	mux.HandleFunc("POST /create-user", createUserHandler)
+	mux.HandleFunc("GET /users", getUserHandler)
+
 
 	fmt.Println("Server is running on port 5000")
-	err:=http.ListenAndServe(":5000", nil)
+	err:=http.ListenAndServe(":5000", mux)
 
 	if err != nil {
 		fmt.Println("Server Error",err)
-	 
 	}
 }
 
 
 func rootHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintln(w,"Welcome to go server")
+}
+
+func healthChecker (w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w, "Server health is good")
+}
+
+func createUserHandler (w http.ResponseWriter, r *http.Request){
+	fmt.Fprintln(w, "User Created Successfully")
+}
+
+func getUserHandler (w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	// formatUsers, _ := json.Marshal(users)
+	// w.Write(formatUsers)
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(users)
+
 }
